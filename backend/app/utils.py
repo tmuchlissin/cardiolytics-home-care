9dfc4e1a6d9358b68f5e9d58b7938b99f41619f7
Toriqul Muchlissin <tmuchlissin@gmail.com>
2025-07-05 16:14:19 +0700

    update:main: fix app

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -12,7 +12,7 @@ from flask import current_app, send_file
 
 from app.extensions import mail
 from app.models import Models, PatientData
-# from app.wrappers import PyTorchClassifier
+from app.wrappers import PyTorchClassifier
 
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Image, Spacer
 from reportlab.lib import colors
@@ -46,36 +46,36 @@ def verify_reset_token(token, expiration=3600):
 ##################### CVD PREDICT #########################
 ###########################################################
 
-# try:
-#     import sklearn._loss._loss as skloss
-# except ImportError:
-#     skloss = None
+try:
+    import sklearn._loss._loss as skloss
+except ImportError:
+    skloss = None
 
-# if skloss is not None:
-#     if not hasattr(skloss, '__pyx_unpickle_CyHalfBinomialLoss'):
-#         def dummy_unpickle_CyHalfBinomialLoss(*args, **kwargs):
-#             from sklearn.metrics import log_loss
-#             return log_loss
-#         setattr(skloss, '__pyx_unpickle_CyHalfBinomialLoss', dummy_unpickle_CyHalfBinomialLoss)
+if skloss is not None:
+    if not hasattr(skloss, '__pyx_unpickle_CyHalfBinomialLoss'):
+        def dummy_unpickle_CyHalfBinomialLoss(*args, **kwargs):
+            from sklearn.metrics import log_loss
+            return log_loss
+        setattr(skloss, '__pyx_unpickle_CyHalfBinomialLoss', dummy_unpickle_CyHalfBinomialLoss)
 
-# class CustomUnpickler(pickle.Unpickler):
-#     def find_class(self, module, name):
-#         if module == "__main__" and name == "PyTorchClassifier":
-#             return PyTorchClassifier
-#         return super().find_class(module, name)
+class CustomUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == "__main__" and name == "PyTorchClassifier":
+            return PyTorchClassifier
+        return super().find_class(module, name)
 
-# def load_active_model():
-#     active_model = Models.query.filter_by(is_active=True).first()
-#     if not active_model:
-#         current_app.logger.error("No active model found in the database")
-#         return None
-#     try:
-#         file_obj = io.BytesIO(active_model.file)
-#         pipeline = CustomUnpickler(file_obj).load()
-#         return pipeline
-#     except Exception as e:
-#         current_app.logger.error(f"[MODEL LOAD ERROR] {e}\n{traceback.format_exc()}")
-#         return None
+def load_active_model():
+    active_model = Models.query.filter_by(is_active=True).first()
+    if not active_model:
+        current_app.logger.error("No active model found in the database")
+        return None
+    try:
+        file_obj = io.BytesIO(active_model.file)
+        pipeline = CustomUnpickler(file_obj).load()
+        return pipeline
+    except Exception as e:
+        current_app.logger.error(f"[MODEL LOAD ERROR] {e}\n{traceback.format_exc()}")
+        return None
     
 ###########################################################
 ##################### MEDICAL REPORT ######################
